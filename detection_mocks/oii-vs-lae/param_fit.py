8f6d769137a6487e46b851031c810c88fa64962d
qezlou <sumqezlou@gmail.com>
2024-10-05 22:34:26 -0500

    MCMC for Oii-vs-LAE
    - Checking the constraints on the combined distance D_v

--- detection_mocks/oii-vs-lae/param_fit.py
+++ detection_mocks/oii-vs-lae/param_fit.py
@@ -19,8 +19,9 @@ if __name__ == '__main__':
     argparser.add_argument('--stack', type=int,  default=0, help='Stack the mocks')
     argparser.add_argument('--data_dir', type=str,  default='/home/qezlou/HD2/HETDEX/cosmo/data/lognormal-mocks/')
     args = argparser.parse_args()
+
     
-    def get_linear_corr(params, fixed_params):
+    def get_fiducial_linear_corr(r, z):
         """
         Parameters
         ----------
@@ -33,47 +34,36 @@ if __name__ == '__main__':
         array
             Array of correlation function values
         """
-        h, sigma8 = params
-        r = fixed_params['r']
-        z = fixed_params['z']
         cosmo = cosmology.Planck15
-        cosmo = cosmo.clone(h=h)
-        Plin = cosmology.LinearPower(cosmo, redshift=z, transfer='EisensteinHu')
-        Plin.sigma8 = sigma8
+        Plin = cosmology.ZeldovichPower(cosmo, redshift=z, transfer='EisensteinHu')
         cf_lin = cosmology.CorrelationFunction(Plin)
         return cf_lin(r)
 
-    def fit_param():
-        param_fit = []
-        for p in param_range:
-            model.set_params(**{param: p})
-            model.fit(data)
-            param_fit.append(model.score(data))
-    
-    def log_prior(params, param_range):
+    def log_prior(params):
         # Define a log-prior function with bounds on the parameters
         for i, p in enumerate(params):
             if (p < param_range[i][0])*(param_range[i][1] > p):
                 return -np.inf # Log of 0, meaning impossible outside the range
         return 0.0 # Log of 1, meaning no penalty within this range
 
-    def log_likelihood(params, fixed_params, data, sigma):
-        # Define a log-likelihood function
-        pred = get_linear_corr(params, fixed_params)
+    def log_likelihood(params):
+        """Define a log-likelihood function"""
+        # ```model = model_fid(alpha * r)```
+        pred =  params[1]*np.interp(data_r, params[0]*fid_r, model_fid)
+        
         return -0.5 * np.sum((data - pred) ** 2 / sigma ** 2)
 
-    def log_posterior(params, fixed_params, param_range, data, sigma):
+    def log_posterior(params):
         # Define the log-posterior function including the prior term
-        log_pr = log_prior(params, param_range)
+        log_pr = log_prior(params)
         if not np.isfinite(log_pr):
             return -np.inf  # If the prior is not finite, return -inf
-        return log_pr + log_likelihood(params, fixed_params, data, sigma)
+        return log_pr + log_likelihood(params)
 
-    def run_mcmc(nwalkers, n_iter, ndim, data, sigma, fixed_params, param_range, pool):
+    def run_mcmc(pool):
 
         sampler = emcee.EnsembleSampler(nwalkers, ndim, 
-                                        log_posterior, 
-                                        args=(fixed_params, param_range, data, sigma),
+                                        log_posterior,
                                         pool=pool)
         pos = np.zeros((nwalkers, ndim))
         for p in range(ndim):
@@ -101,62 +91,54 @@ if __name__ == '__main__':
     print(args.stype, all_corr[args.stype]['z'], args.r_range)
     # Fnd the correlation function values within the range
     ind = np.where((r >= args.r_range[0])*(r <= args.r_range[1]))
-    fixed_params = {'r': r[ind], 'z': all_corr[args.stype]['z']}
-    param_range = [(0.60, 0.70), (0.7, 1.7)]
+    data_r = r[ind]
+    z = all_corr[args.stype]['z']
+    # prior range for alpha and scale
+    param_range = [(0.85, 1.15), (1, 5)]
     # get the percentiles, mostly for sigma which is used in the posterior
     percents = np.array([2.28, 15.87, 50, 84.13, 97.27])
     pcnt = np.percentile(np.array(all_corr[args.stype]['corr']), percents, axis=0)
     sigma = 0.5*(pcnt[3] - pcnt[1])[ind]
+    data = pcnt[2][ind]
 
-    if args.stack:
-        # fit for the median correlation function
-        print('Stacking the mocks')
-        data = pcnt[2]
-        data = data[ind]
-        sigma = sigma[ind]
+    fid_r = np.linspace(np.log10(r[ind][0]), np.log10(r[ind][-1]), 1000)
+    fid_r = 10**fid_r
 
-        if not args.optimize:
-            # Run MCMC
-            nawalkers = 10
-            n_iter = 1_000
-            ndim = 2
-            
-            with Pool() as pool:
-                sampler = run_mcmc(nawalkers, n_iter, ndim, data, sigma, fixed_params, param_range, pool)
-            samples = sampler.get_chain(discard=100, thin=15, flat=True)
+    print(f'data_r = {(data_r[0], data_r[-1])}, fid_r = {(fid_r[0], fid_r[-1])}')
 
-            with h5py.File(op.join(args.data_dir,'samples.hdf5'), 'w') as f:
-                f.create_dataset('samples', data=samples)
-                f.create_dataset('lnprob', data=sampler.get_log_prob(discard=100, thin=15, flat=True))
-                f.create_dataset('acceptance', data=sampler.acceptance_fraction)
-                f.create_dataset('acor', data=sampler.get_autocorr_time())
-                f.create_dataset('params', data=['h', 'sigma8'])
-                f.create_dataset('fixed_params', data=[all_corr[args.stype]['r'], all_corr[args.stype]['z']])
-                f.create_dataset('param_range', data=param_range)
-                f.create_dataset('nwalkers', data=nawalkers)
-                f.create_dataset('n_iter', data=n_iter)
-                f.create_dataset('ndim', data=ndim)
-                f.create_dataset('data', data=data)
-                f.create_dataset('sigma', data=sigma)
-                f.create_dataset('stype', data=args.stype)
-        else:
-            # Get the maximum a posteriori estimate, fatser
-            res = minimize(lambda x: -log_posterior(x, fixed_params, param_range, 
-                                                    data, sigma), x0=[0.65, 1.0], bounds=param_range,
-                                                    options={'disp': False})
-    else:   
-            # Fit for individual correlation functions
-            all_fits = np.zeros((len(all_corr[args.stype]['corr']), 2))
-            for i in tqdm(range(len(all_corr[args.stype]['corr'])), desc="Fitting individual mocks"):
-                data = all_corr[args.stype]['corr'][i][ind]
-                res = minimize(lambda x: -log_posterior(x, fixed_params, param_range,
-                                                        data, sigma), x0=[0.65, 1.0], bounds=param_range,
-                                                        options={'disp': False}).x
-                all_fits[i] = np.array(res)
-            with h5py.File(op.join(args.data_dir,f'fits_{args.stype}_r_{args.r_range[0]}_{args.r_range[1]}.hdf5'), 'w') as f:
-                f.create_dataset('fits', data=all_fits)
-                f.create_dataset('r', data=fixed_params['r'])
-                f.create_dataset('z', data=fixed_params['z'])
-                f.create_dataset('param_range', data=param_range)
-                f.create_dataset('stype', data=args.stype)
+    ## Fiducial Model: Linear power at Planc15 cosmology
+    model_fid = get_fiducial_linear_corr(r=fid_r, z=all_corr[args.stype]['z'])
 
+
+    # fit for the median correlation function
+    print('Stacking the mocks')
+
+    # Run MCMC
+    nwalkers = 10
+    n_iter = 1_000_000
+    # Two parameters to fit: alpha and scale
+    # `alpha`` is the scaling factor for the coordinates
+    # `scale`` is the scaling factor for the correlation function
+    ndim = 2 
+    
+    with Pool() as pool:
+        sampler = run_mcmc(pool)
+    samples = sampler.get_chain(discard=100, thin=15, flat=True)
+
+    save_file = op.join(args.data_dir,f'samples_{args.stype}.hdf5')
+    print(f'saving chains on file : {save_file}')
+    
+
+    with h5py.File(save_file, 'w') as f:
+        f.create_dataset('samples', data=samples)
+        f.create_dataset('lnprob', data=sampler.get_log_prob(discard=100, thin=15, flat=True))
+        f.create_dataset('acceptance', data=sampler.acceptance_fraction)
+        f.create_dataset('acor', data=sampler.get_autocorr_time())
+        f.create_dataset('params', data=['alpha'])
+        f.create_dataset('param_range', data=param_range)
+        f.create_dataset('nwalkers', data=nwalkers)
+        f.create_dataset('n_iter', data=n_iter)
+        f.create_dataset('ndim', data=ndim)
+        f.create_dataset('data', data=data)
+        f.create_dataset('sigma', data=sigma)
+        f.create_dataset('stype', data=args.stype)
