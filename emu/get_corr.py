86cc302d59e2434d82c0a26732bc11e8797494bf
Qezlou <mahdiqezlou@gmail.com>
2024-10-16 17:14:22 -0500

    seed effect on corr func
    - Cleaning up the code for getting corr from Goku FOF files
    - HOD seed apparantly has a large effect on correlation function, now
      we get ~ 100 realization
    - [ ] I need to find a way how to incorporate this uncertainty

--- emu/get_corr.py
+++ emu/get_corr.py
@@ -13,13 +13,15 @@ from nbodykit.lab import *
 from nbodykit.cosmology import Planck15 as cosmo
 import h5py
 import logging
+import warnings
+warnings.filterwarnings("ignore")
 
 comm = CurrentMPIComm.get()
 rank = comm.Get_rank()
 size = comm.Get_size()
 
 class Corr():
-    def __init__(self, logging_level):
+    def __init__(self, logging_level='INFO'):
         self.logger = self.configure_logging(logging_level)
 
     def configure_logging(self, logging_level):
@@ -41,7 +43,7 @@ class Corr():
 
 
     def get_pig_dirs(self, base_dir, z=2.5):
-        """Get the directories of the PIGs
+        """Get the directories of the PIGs at redshift z
         Parameters
         ----------
         base_dir : str
@@ -72,7 +74,17 @@ class Corr():
                 pigs['params'].append(json.load(f))
         return pigs
 
-    def load_hlo_cat(self, pig_dir):
+    def load_halo_cat(self, pig_dir):
+        """
+        Load FOF tables as Nbodykit Halocatalog. It will be used to populate them with HOD models.
+        Parameters:
+        ------------
+        pig_dir: str, 
+            The path to the PIG directory
+        Returns:
+        -----------
+        Nbodykit HaloCatalog
+        """
         cat = BigFileCatalog(pig_dir, dataset='FOFGroups')
         redshift = 1/cat.attrs['Time'] - 1
         cat['Mass'] *= 1e10
@@ -89,70 +101,97 @@ class Corr():
 
     def populate_halos(self, pig_dir, hod_model, seed=42, model_params={}):
         """
-        
+        Populate a PIG (FOF table) with central and satelite galaxies with HOD of choice
+        Paramters:
+        -----------
+        pig_dir: str, 
+            The path to the PIG directory
+        hod_model: A class from `Nbodykit.hod`
+            The models defined in `Nbodykit.hod`
+        seed: int,
+            The random seed to fix the HOD realization
+        model_params:
+            The parameters to be passed to the HOD model
+        Returns:
+        ----------
+        hod catalog in `Nbodykit`'s formate
         """
-        halos = self.load_hlo_cat(pig_dir)
+        halos = self.load_halo_cat(pig_dir)
         hod = halos.populate(hod_model, seed=seed, **model_params)
-        self.logger.info("total number of HOD galaxies = ", hod.csize)
-        print(hod.columns)
+        self.logger.info(f"total number of HOD galaxies = {hod.csize}")
 
-        self.logger.debug(f"number of centrals = { hod.compute((hod['gal_type']==0).sum())}")
-        self.logger.debug(f"number of satellites = {hod.compute((hod['gal_type']==1).sum())}")
-
-
-        #cen_idx = hod['gal_type'] == 0
-        #sat_idx = hod['gal_type'] == 1
-
-        #cens = hod[cen_idx]
-        #sats = hod[sat_idx]
-
-        #return hod, cens, sats
         return hod
 
 
-    def get_corr(self, pig_dir, r_edges, seed=42, corr_mode='1d', hod_model=Zheng07Model):
-        """Plot the corrfunction of the galaxies, centrals, and satellites.
+    def get_corr(self, pig_dir, r_edges, seeds=[42], corr_mode='1d', hod_model=Zheng07Model, model_params={}):
+        """Get the correlation function for HOD populated galaxies in a FOF halo catalog.
         Parameters
         ----------
-        seed : int
-            seed for the HOD model
+        pig_dir: str, 
+            The path to the PIG directory
+        r_egses: array,
+            binning along r
+        seeds : list/array
+            seeds for the HOD model, if len(seeds) > 1, generate different realizations
+            of HOD populated galaxies
         corr_mode : str
             mode of the correlation function, either '1d', '2d', 'projected', 'angular'
+        hod_model: A class from `Nbodykit.hod`
+            The models defined in `Nbodykit.hod`
+        Returns:
+        ------------
+        The correlation function for different seeds
         """
-        #hod, cens, sats = populate_halos(seed=seed, hod_model=hod_model)
-        # in real space
-        #corr_gal_real = SimulationBox2PCF(data1=hod, mode=corr_mode, edges=r_edges, position='Position')
-        #corr_gal_real.run()
-        hod = self.populate_halos(pig_dir, hod_model=hod_model, seed=seed)
         # in z-space
-        LOS = [0, 0, 1]
-        hod['RSDPosition'] = hod['Position'] + hod['VelocityOffset'] * LOS
-        corr_gal_zspace = SimulationBox2PCF(data1=hod, mode=corr_mode, edges=r_edges,  position='RSDPosition')
-        corr_gal_zspace.run()
+        los = [0, 0, 1]
+        halos = self.load_halo_cat(pig_dir)
+        all_corrs = np.zeros((seeds.size, len(r_edges)-1))
+        for i, sd in enumerate(seeds):
+            if i==0:
+                hod = halos.populate(hod_model, seed=sd, **model_params)
+            else:
+                hod.repopulate(seed=sd)
+        
+            # Apply RSD to the galaxies
+            hod['RSDPosition'] = hod['Position'] + hod['VelocityOffset'] * los
+            corr_gal_zspace = SimulationBox2PCF(data1=hod, mode=corr_mode, edges=r_edges,  position='RSDPosition')
+            corr_gal_zspace.run()
+            all_corrs[i] = corr_gal_zspace.corr['corr'][:]
+        return all_corrs
 
-        #cen_idx = hod['gal_type'] == 0
-        #sat_idx = hod['gal_type'] == 1
-        #cens = hod[cen_idx]
-        #sats = hod[sat_idx]
-        #corr_cen_zspace = SimulationBox2PCF(data1=cens, mode=corr_mode, edges=r_edges, position='RSDPosition')
-        #corr_cen_zspace.run()
-        #corr_sat_zspace = SimulationBox2PCF(data1=sats, mode=corr_mode, edges=r_edges, position='RSDPosition')
-        #corr_sat_zspace.run()
-
-        return corr_gal_zspace
-
-    def iterate_over_pigs(self, base_dir,  z=2.5, savedir = '/work2/06536/qezlou/Goku/corr_funcs'):
-
+    def fix_hod_all_pigs(self, base_dir, seeds=[42], z=2.5, savedir = '/work2/06536/qezlou/Goku/corr_funcs'):
+        """
+        Iterate over all FOF Catalogs at redshift z in `base_dir` directory keeping
+        HOD paramters the same. We get manny realizations of the HOD populated catalogs.
+        Parameters:
+        -------------
+        base_dir: str,
+            The directory containing the FOF catalogs
+        z: int,
+            The redshift of interest
+        savedir:
+            The directory to save the correlation functions at
+        """
         hod_model = Zheng07Model
         pigs = self.get_pig_dirs(base_dir, z=z)
         r_edges = np.logspace(0, np.log10(200), 100)
-        corr_func = np.zeros((len(pigs['sim_tags']), len(r_edges)-1))
         for i, pig_dir in enumerate(pigs['pig_dirs']):
-            corr_gal_zspace = self.get_corr(pig_dir, r_edges, seed=42, hod_model=hod_model)
-            corr_func[i] = corr_gal_zspace.corr['corr']
-        with h5py.File(os.path.join(savedir, f'corr_gal_zspace_z{np.round(z,2)}.hdf5'), 'w') as f:
-            f['r'] = ( r_edges[:-1] + r_edges[1:] ) / 2
-            f['corr'] = corr_func
+            if rank==0:
+                self.logger.info(f"pig_dir = {pig_dir} | progress = {np.round(i/len(pigs['pig_dirs']), 2)*100} %")
+            try:
+                corr = self.get_corr(pig_dir, r_edges, seeds=seeds, hod_model=hod_model)
+            except ValueError:
+                print(f'Issues with {pig_dir}')
+                continue
+            
+            save_file=f'Zheng07_seeds_{pigs["sim_tags"][i]}.hdf5'
+            comm.Barrier()
+            if rank ==0:
+                with h5py.File(os.path.join(savedir, save_file), 'w') as f:
+                    f['r'] = ( r_edges[:-1] + r_edges[1:] ) / 2
+                    f['corr'] = corr
+                    f['seeds'] = seeds
+            comm.Barrier()
 
 
 if __name__ == '__main__':
@@ -161,9 +200,15 @@ if __name__ == '__main__':
     parser.add_argument('--base_dir', type=str, help='Base directory of the simulations')
     parser.add_argument('--z', type=float, help='Redshift of the snapshot')
     parser.add_argument('--savedir', type=str, help='Directory to save the correlation functions')
-    parser.add_argument('--logging_level', required=False, type=int, default=logging.INFO, help='Logging level')
+    parser.add_argument('--logging_level', required=str, type=str, default=logging.INFO, help='Logging level')
     args = parser.parse_args()
+    if args.logging_level == 'INFO':
+        args.logging_level = 20
+    elif args.logging_level == 'DEBUG':
+        args.logging_level = 10
     corr = Corr(args.logging_level)
-    corr.iterate_over_pigs(args.base_dir, z=args.z, savedir=args.savedir)
 
-
+    # Get the Corrs for fixed HOD parameters of Zheng+07, but difference seeds
+    # for all avaialble cosmologies
+    seeds = np.unique(np.random.randint(0, 1000_000, size=100))
+    corr.fix_hod_all_pigs(args.base_dir, seeds=seeds, z=args.z)
