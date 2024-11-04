ab4b1a16b9194b0b667509df95cc13579277c34d
Qezlou <mahdiqezlou@gmail.com>
2024-11-04 15:13:38 -0600

    halo mass function

---
+++ emu/hmf.py
@@ -0,0 +1,63 @@
+import os
+import argparse
+import json
+from glob import glob
+import numpy as np
+import re
+
+from nbodykit import CurrentMPIComm
+from nbodykit.hod import Zheng07Model
+from nbodykit.lab import *
+from nbodykit.cosmology import Planck15 as cosmo
+import h5py
+import logging
+import warnings
+import mpi_helper
+import get_corr
+warnings.filterwarnings("ignore")
+
+class Hmf(get_corr.Corr):
+    def __init__(self, logging_level='INFO', ranks_for_nbkit=0):
+        super().__init__(logging_level, ranks_for_nbkit)
+        self.logger = logging.getLogger('Hmf')
+        self.logger.setLevel(logging_level)
+    
+    def configure_logging(self, logging_level):
+        """Sets up logging based on the provided logging level."""
+        logger = logging.getLogger('Hmf')
+        logger.setLevel(logging_level)
+        try:
+            from nbodykit import setup_logging
+            setup_logging('info')
+        except ImportError:
+            console_handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
+        if self.rank==0:
+            logger.info('Logger initialized at level: %s', logging.getLevelName(logging_level))
+            logger.info(f'MPI_COMM_WORLD | size = {self.size} -- Nbkit COMM | size = {self.nbkit_size}')
+        
+        return logger
+    
+    def get_fof_hmf(self, pig_dir, bins=100,  z=2.5):
+        """
+        Plot the halo mass function for the FoF halos
+        Parameters:
+        -----------
+        pig_dir: str
+            The directory containing the PIGs
+        z: float
+            The redshift of the snapshot
+        """
+        halos = self.load_halo_cat(pig_dir)
+        hist = np.histogram(halos['Mass'], bins=bins, range=(np.min(halos['Mass']), np.max(halos['Mass'])))
+        mass = 0.5*(hist[1][1:]+hist[1][:-1])
+
+        return mass, hist[0]
+    
+    def get_all_fof_hmfs(self, base_dir, z=2.5):
+        """iterate over all avaiable pigs in base_dir and co,pue the halo mas function"""
+
+        pigs = self.get_pig_dirs(base_dir, z=z)
+        
