1334fba258a979deaf24c683747e573a360523b8
Qezlou <mahdiqezlou@gmail.com>
2025-02-18 15:39:20 -0600

    util update untar
    Extracts all the tar files in the current directory

--- utils/untar.sh
+++ utils/untar.sh
@@ -1,27 +1,44 @@
-prefix=$1  # First argument: file name prefix e.g. cosmo_10p_Box250_Part750_
-lower=$2   # Second argument: lower bound
-upper=$3   # Third argument: upper bound
+#!/bin/bash
 
-batch_size=10  # Define batch size
-current_lower=$lower
+num_jobs=3  # Number of parallel jobs
+tar_files=(*.tar)
+total_files=${#tar_files[@]}
 
-while [ $current_lower -le $upper ]; do
-    current_upper=$((current_lower + batch_size - 1))
-    if [ $current_upper -gt $upper ]; then
-        current_upper=$upper  # Cap the upper bound to the final value
-    fi
+if [ "$total_files" -eq 0 ]; then
+    echo "No .tar files found in the current directory."
+    exit 1
+fi
 
-    # Name the screen session based on current batch
-    screen_name="L2-untar-$current_lower-$current_upper"
-    
-    # Run the untar script inside a new screen session
-    screen -dmS "$screen_name" bash -c "
-    for i in \$(seq $current_lower $current_upper); do
-        formatted_num=\$(printf \"%04d\" \"\$i\")
-        tar -xvf \"${prefix}\${formatted_num}.tar\"
-    done
-    "
-
-    # Move to the next batch
-    current_lower=$((current_upper + 1))
+# Build a string that redefines the tar_files array in the screen session.
+# This simple approach works as long as your file names don't contain spaces.
+tar_files_list=""
+for file in "${tar_files[@]}"; do
+    tar_files_list+="'${file}' "
 done
+
+# Function to process files inside a screen session
+extract_files() {
+    local job_id=$1
+    local screen_name="untar-job-$job_id"
+
+    # Start a new screen session and run the extraction.
+    # We embed the array definition and other needed variables into the command.
+    screen -S "$screen_name" -dm bash -c "tar_files=(${tar_files_list});
+total_files=\${#tar_files[@]};
+for ((j=${job_id}; j<total_files; j+=${num_jobs})); do
+    echo \"Extracting \${tar_files[j]} (Job ${job_id})...\";
+    tar -xvf \"\${tar_files[j]}\";
+done;
+echo \"Extraction completed in ${screen_name}\";
+exec bash"  # Keeps the screen session open after completion.
+}
+
+# Start jobs in separate screen sessions
+for ((i = 0; i < num_jobs; i++)); do
+    extract_files $i
+done
+
+echo "Extraction started in $num_jobs screen sessions."
+echo "Use 'screen -ls' to list running sessions."
+echo "Attach to a session with: screen -r untar-job-<ID>"
+echo "For example: screen -r untar-job-0"
