1ab947f71f7ca98470a6aac1a2d0487c957f7dce
Qezlou <mahdiqezlou@gmail.com>
2025-02-23 14:39:35 -0600

    xi sims: cleanup

--- emu/slurm_files/submit_corr_grid.py
+++ emu/slurm_files/submit_corr_grid.py
@@ -2,7 +2,7 @@ import os
 
 # Define the template for the modified lines
 template = """#!/bin/bash
-#SBATCH -J mg{i}H
+#SBATCH -J mg{i}
 #SBATCH -p skx
 #SBATCH -N 1
 #SBATCH --ntasks-per-node 48
@@ -11,11 +11,11 @@ template = """#!/bin/bash
 
 hostname; pwd; date
 
-ibrun python run_xi_train.py --fid 'HF' --narrow 0 --numchunks 21 --chunk {i}
+ibrun python run_xi_train.py --fid 'L2' --narrow 0 --numchunks 20 --chunk {i}
 """
 
 # Loop from 0 to 20 and create modified files
-for i in range(1,18):
+for i in range(0,6):
     print(i)
     filename = f"job_script_{i}.sh"
     with open(filename, "w") as f:
