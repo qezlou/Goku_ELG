9f6242dac0f3fbab1eb5554c36cdbd7a9a81256d
qezlou <sumqezlou@gmail.com>
2025-03-26 10:50:26 -0500

    code for xi emu err vs mass pair

--- emu/slurm_files/xi_emu/path2_loo_err.py
+++ emu/slurm_files/xi_emu/path2_loo_err.py
@@ -6,39 +6,64 @@ from gal_goku import emus_multifid
 from gal_goku import summary_stats
 importlib.reload(emus_multifid)
 import os.path as op
+import argparse
 import pickle
 import warnings
 warnings.filterwarnings('ignore')
+from gal_goku import mpi_helper
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
 
-def loo_mean_err_wide_narrow():
-    data_dir = '/home/qezlou/HD2/HETDEX/cosmo/data/xi_on_grid/'
+def loo_mean_err_wide_narrow(data_dir = '/home/qezlou/HD2/HETDEX/cosmo/data/xi_on_grid/'):
 
     # Get a list of mass_pairs
     xi = summary_stats.Xi(data_dir, fid='HF', logging_level='ERROR')
     all_mass_pairs = xi.mass_pairs
-    all_frac_errs = np.full((len(all_mass_pairs), 36, 26), np.nan)
-    for m, mass_pair in enumerate(all_mass_pairs):
-        print(f'mass_pair: {mass_pair}')
-        frac_err_mass_pair = []
-        for s in range(36):
+    all_frac_errs = np.zeros((len(all_mass_pairs), 36, 26), dtype=np.float32)
+    starts, ends = mpi_helper.into_chunks(comm, all_mass_pairs.shape[0])
+    s, e = starts[rank], ends[rank]
+    bad_sims = np.zeros((len(all_mass_pairs), 36), dtype=bool)
+    print(f'rank {rank} | s: {s}, e: {e}', flush=True)
+    for m in range(s, e):
+        print(f'rank {rank} | mass_pair: {all_mass_pairs[m]}', flush=True)
+        for sim in range(36):
             row, col = divmod(s, 4)
-            model_file = f'Xi_Native_emu_mapirs2_spline_{mass_pair[0]}_{mass_pair[1]}_wide_narrow_leave_{s}.pkl'
+            model_file = f'Xi_Native_emu_mapirs2_spline_{all_mass_pairs[m][0]}_{all_mass_pairs[m][1]}_wide_narrow_leave_{sim}.pkl'
             emu_type = {'wide_and_narrow':True}
         
             # Plot true vs prediction
-            xi_emu = emus_multifid.XiNativeBins(data_dir, interp='spline', mass_pair=mass_pair, logging_level='ERROR', emu_type=emu_type)
+            xi_emu = emus_multifid.XiNativeBins(data_dir, interp='spline', mass_pair=all_mass_pairs[m], logging_level='ERROR', emu_type=emu_type)
             # Make sure the test sim is not missing
             try:
                 # Predict
-                mean, var = xi_emu.predict(ind_test=np.array([s]), model_file=model_file)
-                all_frac_errs[m, s, :] = 10**mean[0]/10**xi_emu.Y[1][s] -1
+                mean, var = xi_emu.predict(ind_test=np.array([sim]), model_file=model_file)
+                all_frac_errs[m, sim, :] = 10**mean[0]/10**xi_emu.Y[1][sim] -1
                 rbins = xi_emu.mbins
             except FileNotFoundError:
+                bad_sims[m, sim] = True
                 print(f'{model_file} not found')
-            
     
-    with h5py.Fileop.join(data_dir, 'train', 'median_loo_err.hdf5', 'w') as f:
-        f.create_dataset('frac_errs', data=all_frac_errs)
-        f.create_dataset('rbins', data=rbins)
+    comm.Barrier()
+    # MPI communication
+    ## bad sims
+    bad_sims = np.ascontiguousarray(bad_sims, dtype=bool)
+    comm.Allreduce(MPI.IN_PLACE, bad_sims, op=MPI.LOR)
+    # all the frac errs
+    all_frac_errs = np.ascontiguousarray(all_frac_errs, dtype=np.float32)
+    comm.Allreduce(MPI.IN_PLACE, all_frac_errs, op=MPI.SUM)
+    if rank == 0:
+        all_frac_errs[bad_sims] = np.nan
+        print('Saving...', flush=True)
+        with h5py.File(op.join(data_dir, 'train', 'median_loo_err.hdf5'),'w') as f:
+            f.create_dataset('frac_errs', data=all_frac_errs)
+            f.create_dataset('rbins', data=rbins)
+    comm.Barrier()
 
-loo_mean_err_wide_narrow()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str)
+    args = parser.parse_args()
+    loo_mean_err_wide_narrow(args.data_dir)
