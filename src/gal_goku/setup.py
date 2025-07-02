40969a61e52c8e4dbb7c92f46a9c03ff88e270d0
Qezlou <qezlou@austin.utexas.edu>
2025-07-02 18:13:21 -0500

    install `classylss` in `setup.py`

--- src/gal_goku/setup.py
+++ src/gal_goku/setup.py
@@ -1,8 +1,73 @@
 import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+import subprocess
+import os
+import shutil
 
+class CustomInstallCommand(install):
+    """Customized install command - clones repos, copies files, and installs classylss."""
+
+    def run(self):
+        self.do_custom_steps()
+        super().run()
+
+    def do_custom_steps(self):
+        parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
+        print(f'Parent directory: {parent_dir}', flush=True)
+
+        # Clone classylss repository
+        classylss_repo = os.path.join(parent_dir, 'classylss')
+        if not os.path.exists(classylss_repo):
+            print('Cloning classylss repository...', flush=True)
+            subprocess.check_call(['git', 'clone', 'https://github.com/sbird/classylss.git', classylss_repo])
+        else:
+            print(f'{classylss_repo} already exists, skipping clone.', flush=True)
+
+        # Clone class_public repository
+        class_public_repo = os.path.join(parent_dir, 'class_public')
+        if not os.path.exists(class_public_repo):
+            print('Cloning class_public repository...', flush=True)
+            subprocess.check_call(['git', 'clone', 'https://github.com/lesgourg/class_public.git', class_public_repo])
+        else:
+            print(f'{class_public_repo} already exists, skipping clone.', flush=True)
+
+        # Copy ./class_public/external/ to ./classylss/classylss/data/
+        src_dir = os.path.join(class_public_repo, 'external')
+        dest_dir = os.path.join(classylss_repo, 'classylss', 'data')
+
+        if os.path.exists(src_dir):
+            if not os.path.exists(dest_dir):
+                os.makedirs(dest_dir)
+            s = os.path.join(src_dir, 'bbn')
+            d = os.path.join(dest_dir, 'bbn')
+            if os.path.isdir(s):
+                shutil.copytree(s, d, dirs_exist_ok=True)
+            else:
+                shutil.copy2(s, d)
+            print('File bbn copied successfully.', flush=True)
+        else:
+            print(f'Source directory {src_dir} does not exist.', flush=True)
+
+        # Install classylss
+        subprocess.check_call(['python', '-m', 'pip', 'install', classylss_repo])
+
+
+class CustomDevelopCommand(develop):
+    """Custom develop command - same as custom install."""
+
+    def run(self):
+        self.do_custom_steps()
+        super().run()
+
+    def do_custom_steps(self):
+        CustomInstallCommand.do_custom_steps(self)
+
+# Read the long description from README.md
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Setup function
 setuptools.setup(
     name="gal_goku",
     version="0.0.0",
@@ -21,12 +86,17 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(where="."),
-    package_dir={"": "."},  # Explicitly tell setuptools that packages are inside 'src/'
+    package_dir={"": "."},
     python_requires=">=3.9",
     install_requires=[
-    "scipy",
-    "numpy",
-    "h5py",
-    "mcfit",
+        "scipy",
+        "numpy",
+        "h5py",
+        "mcfit",
+        "cython"
     ],
+    cmdclass={
+        'install': CustomInstallCommand,  # This will automatically trigger during installation
+        'develop': CustomDevelopCommand,  # This will automatically trigger during development mode
+    },
 )
